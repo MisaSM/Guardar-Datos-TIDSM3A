f75811df4ef1233f1e21c3f1d8a7289bb00ba2db
MisaSM <misaelsarabiamolina@gmail.com>
2022-08-12 16:27:58 -0700

    Funcionalidad de editar añadida

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -35,6 +35,35 @@ router.post('/user_new',(req,res)=>{
     fs.writeFileSync('src/user.json',json_user,'utf-8');
     res.redirect('/');
 })
+
+router.get('/get/:id' ,(req,res)=>{
+    const currentData = JSON.parse(fs.readFileSync('src/user.json'));
+    //console.log(currentData);
+    let newUserData = currentData.filter(user=> {
+        return req.params.id == user.id;
+    });
+    console.log(newUserData);
+    res.render('edituser', newUserData [0]);
+});
+
+
+router.post('/edituser',(req,res)=>{
+    const currentData = JSON.parse(fs.readFileSync('src/user.json'));
+    currentData.forEach(user=> {
+        if(user.id == req.body.id) {
+            user.name = req.body.name.length == 0 ? user.name : req.body.name;
+            user.phone = req.body.phone.length == 0 ? user.phone : req.body.phone;
+            user.email = req.body.email.length == 0 ? user.email : req.body.email;
+            user.description = req.body.description.length == 0 ? user.description : req.body.email;
+        } 
+
+    });
+    fs.writeFileSync('src/user.json', JSON.stringify(currentData), 'utf-8');
+    res.redirect('/users');
+    console.log(currentData);
+    console.log(req.body.name.length);
+});
+
 router.get('/delete/:id',(req,res)=>{
     const currentData = JSON.parse(fs.readFileSync('src/user.json'));
     console.log(currentData);
